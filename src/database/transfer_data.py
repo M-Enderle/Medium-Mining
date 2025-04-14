86796963782cb6f5532720c52448f3f417b61cb2
Moritz Enderle <contact@moritzenderle.com>
2025-04-14 20:49:05 +0200

    refactor: improve import formatting for better readability

--- src/database/transfer_data.py
+++ src/database/transfer_data.py
@@ -3,8 +3,15 @@ from time import time
 
 from tqdm import tqdm
 
-from database import (URL, Base, SessionLocal, Sitemap, create_engine,
-                      sessionmaker, setup_database)
+from database import (
+    URL,
+    Base,
+    SessionLocal,
+    Sitemap,
+    create_engine,
+    sessionmaker,
+    setup_database,
+)
 
 # Database file path
 DATABASE_PATH = "medium_articles.db"  # Replace with your desired database file
